a67b021dd9e7bd8c2e590fd8fa56ce2d13367172
okondo-dell <okondofrank2017@gmail.com>
2023-03-16 21:16:23 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - main entry point
+ *main - Entry point
+ *
  *return : always 0 (success)
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
-	retuern(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	retuern (0);
 }

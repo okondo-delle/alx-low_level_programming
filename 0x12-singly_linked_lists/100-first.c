4364844b62105d28aa3ef9275b92c56dc3d2f1f1
dennisolang <dennisolang@gmail.com>
2023-04-27 22:12:48 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,9 @@ void __attribute__((constructor)) hare(void);
 /**
  *  * hare - Prints a string before the
  *   *        main function is executed.
-*/
+ */
 void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
+}

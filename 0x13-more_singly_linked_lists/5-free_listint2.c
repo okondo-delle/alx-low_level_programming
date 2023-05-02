88d54849c6ff221d83d0946c732f4a398fd59929
dennisolang <dennisolang@gmail.com>
2023-05-02 21:50:39 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,6 +4,8 @@
  *  * free_listint2 - Frees a listint_t list.
  *   * @head: A pointer to the address of the
  *    *        head of the listint_t list.
+ *     *
+ *      * Description: Sets the head to NULL.
 */
 void free_listint2(listint_t **head)
 {
@@ -20,4 +22,4 @@ void free_listint2(listint_t **head)
 	}
 
 	head = NULL;
-
+}
